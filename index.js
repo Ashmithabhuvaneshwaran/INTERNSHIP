46acd3679c936ea284b510826c65f491bd8158dd
Ashmithabhuvaneshwaran <147639319+Ashmithabhuvaneshwaran@users.noreply.github.com>
2024-08-12 10:28:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,77 @@
-const file=require('fs') 
-let filedata=file.readFileSync('data.txt').toString()
-console.log(filedata);
+const express=require('express');
+
+
+const mongoose=require('mongoose')
+
+
+// step-1 connecting with mongoose
+async function connect()
+{
+  await mongoose.connect("mongodb+srv://ashmithabhuvaneshwaran:8Du5yW3nMq09r7Qp@ashmitha.bcikj.mongodb.net/");
+  console.log("db connected");
+}
+
+
+connect()
+
+
+const server=express();
+// step-2 creating schema
+const Phonebook= new mongoose.Schema(
+   {
+       name:String,
+       phone:String,
+       location:String
+   },
+   {
+     collection:'PHONEBOOK'
+   }
+)
+
+
+// step-3 creating model
+const Model= mongoose.model('PHONEBOOK',Phonebook)
+
+server.get('/',(req,res)=>{
+   res.send("app is working fine")
+
+
+})
+
+server.get('/db',async (req,res)=>{
+       const data = await Model.find({});
+       res.json(data)
+})
+
+server.get('/db/:id', async (req,res)=>{
+       const data= await Model.findOne({ _id: req.params.id })
+       // console.log(data);
+       res.json(data)
+})
+
+// server.post(‘/postdata’)
+server.get('/postdata', async (req,res)=>{
+   const data=new Model()   // step1 creating instance of model
+   // step-2 declaring data using model instance (data)
+   data.name="ashmita"
+   data.phone="99988811212"
+   data.location="gobi"
+
+
+   // step-3 saving the data created
+   await data.save()
+   console.log("data saved");
+   res.json(data)
+
+
+})
+
+
+// server.delete(‘/delete/:id’)
+server.get('/delete/:id',async (req,res)=>{
+       await Model.deleteOne({ _id : req.params.id})
+})
+
+server.listen(8000,()=>{
+   console.log("server is running");
+})
